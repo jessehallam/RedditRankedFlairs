8ae53be94377cadfa994b8cf88b2e22a18ec1325
Jesse Hallam <jesse.colin.hallam@gmail.com>
2016-01-03 10:29:39 -0500

    Changed behavior of flair generator.

--- Hallam.RedditRankedFlairs.Infrastructure/LeagueUtil.cs
+++ Hallam.RedditRankedFlairs.Infrastructure/LeagueUtil.cs
@@ -12,8 +12,9 @@ namespace Hallam.RedditRankedFlairs
         public static string Stringify(LeagueInfo league)
         {
             if (league == null) return "";
-            if (league.UpdatedTime.HasValue == false || league.Division == 0) return "";
+            if (league.UpdatedTime.HasValue == false) return "";
+            if (league.Division == 0 || league.Tier == TierName.Unranked) return "Unranked";
             return league.Tier.ToString() + " " + DivisionNames[league.Division - 1];
-        } 
+        }
     }
 }
