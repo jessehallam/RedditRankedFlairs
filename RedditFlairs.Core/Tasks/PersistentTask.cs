cff0420c01d0ffef32fdf2be118d3e71c8b9083f
Jesse Hallam <jesse.hallam@pointalliance.com>
2019-03-15 14:36:24 -0400

    Fixes long-running transactions causing uncontrolled log file growth.

--- RedditFlairs.Core/Tasks/PersistentTask.cs
+++ RedditFlairs.Core/Tasks/PersistentTask.cs
@@ -6,6 +6,8 @@ namespace RedditFlairs.Core.Tasks
 {
     public class PersistentTask<TTask> : IAsyncTask where TTask : IAsyncTask
     {
+        private static readonly TimeSpan MaximumRuntime = TimeSpan.FromMinutes(4.9);
+
         private readonly IServiceProvider serviceProvider;
 
         public PersistentTask(IServiceProvider serviceProvider)
@@ -15,8 +17,19 @@ namespace RedditFlairs.Core.Tasks
 
         public async Task ExecuteAsync()
         {
+            var startTime = DateTimeOffset.UtcNow;
+
             while (true)
             {
+                var now = DateTimeOffset.UtcNow;
+                var runTime = now - startTime;
+
+                // A new job instance will get started in moments after.
+                // This fixes a "feature" of HangFire which causes it to hold open
+                // a SQL transaction for the lifetime of the job, causing log files
+                // to grow out of control.
+                if (runTime > MaximumRuntime) break;
+
                 using (var scope = serviceProvider.CreateScope())
                 {
                     var asyncTask = scope.ServiceProvider.GetRequiredService<TTask>();
